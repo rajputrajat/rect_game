739c0378780560c4f3c58b1b4c3e28c3d617cea9
Rajat Rajput <rajputrajat@gmail.com>
2022-01-01 16:12:37 +0530

    draw a circle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("rect_game")]
+async fn main() {
+    loop {
+        clear_background(DARKGREEN);
+        draw_circle(
+            screen_width() / 2_f32,
+            screen_height() / 2_f32,
+            50_f32,
+            GOLD,
+        );
+        next_frame().await
+    }
 }
